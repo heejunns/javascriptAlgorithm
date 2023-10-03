4a7e0827a4e75a1288c6bcb80cbc8b66c1eb020a
heejunns <wns2252@hanmail.net>
2023-10-03 23:48:01 +0900

    선택 정렬 구현

--- sort/selectSort.js
+++ sort/selectSort.js
@@ -1,3 +1,23 @@
-let data = [5, 3, 4, 1, 2, 3, 21, 2, 1, 51, 2, 3];
+// 선택 정렬
+let data = [5, 3, 4, 1, 2, 20, 21, 102, 6, 51, 92, 35];
 
-const selectSortFunc = (arr) => {};
+const selectSortFunc = (arr) => {
+  for (let i = 0; i < arr.length; ++i) {
+    let currentMin = arr[i];
+    let currentMinIndex = i;
+    for (let j = i + 1; j < arr.length; ++j) {
+      if (currentMin > arr[j]) {
+        currentMin = arr[j];
+        currentMinIndex = j;
+      }
+    }
+    if (arr[i] !== currentMin) {
+      let temp = arr[i];
+      arr[i] = currentMin;
+      arr[currentMinIndex] = temp;
+    }
+  }
+  return arr;
+};
+
+console.log("선택 정렬", selectSortFunc(data));
